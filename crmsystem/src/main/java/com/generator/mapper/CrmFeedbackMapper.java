c959bcb84ab6720ab6c65f545e96a92ff0fed9ed
zhaosong <1799240625@qq.com>
2019-03-20 10:41:34 +0800

    优化1.5

--- crmsystem/src/main/java/com/generator/mapper/CrmFeedbackMapper.java
+++
@@ -1,7 +0,0 @@
-package com.generator.mapper;
-
-import com.crm.graduation.crmsystem.model.utils.MyMapper;
-import com.generator.model.CrmFeedback;
-
-public interface CrmFeedbackMapper extends MyMapper<CrmFeedback> {
-}
